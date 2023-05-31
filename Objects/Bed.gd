b26428355084364f272f6642b24fd4dbe6f131f9
chenqi <1535985458@qq.com>
2023-06-01 02:45:20 +0800

    refactor: health system

--- Objects/Bed.gd
+++ Objects/Bed.gd
@@ -3,25 +3,23 @@ extends Sprite2D
 # 导入 HealthSystem 类型
 const HealthSystem = preload("res://Scripts/HealthSystem.gd")
 
-@onready var healthBar : ProgressBar = $ProgressBar
-var healthSystem : HealthSystem
+@onready var collisionShape : CollisionShape2D = $Hutbox/CollisionShape2D
+@onready var healthSystem  = $HealthSystem
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	healthSystem = $HealthSystem
-	healthSystem.connect("healthChanged", Callable(self, "_onHealthChanged"))
-	healthSystem.setHealthBar(healthBar)
+	pass
 
+# 死亡
+func _onDestroyed():
+	pass
+
+# 生命值变化
 func _onHealthChanged(health: int):
 	# 处理生命值变化的逻辑
-	var maxHealth = healthSystem.maxHealth
-	var healthPercentage = float(health) / maxHealth
-	print("当前生命值：", health)
+	pass
 
-func damagePlayer(damage: int):
-	healthSystem.takeDamage(damage)
 
-func healPlayer(amount: int):
-	healthSystem.heal(amount)
 
 func _on_hutbox_hurt():
-	damagePlayer(20)
+	healthSystem.minusHealth(20)
+
--- Scripts/HealthSystem.gd
+++ Scripts/HealthSystem.gd
@@ -1,15 +1,24 @@
 extends Node
-
+class_name  HealthSystem
 @export var maxHealth : int = 100
 @export var regenRate: float = 5.0  # 每秒回血的速率
 @export var regenDelay: float = 3.0  # 未受攻击时开始回复血量的延迟时间
-
-var currentHealth : int = maxHealth
+@export var healthBar: ProgressBar  # 血条节点
+var currentHealth : int = maxHealth :
+	set(value):
+		currentHealth = clamp(value, 0, maxHealth)
+		updateHealthBar()
+		if currentHealth == 0:
+			emit_signal("destroyed")
+			owner.queue_free()
+		if currentHealth == maxHealth:
+			stopRegenTimer()
+			
 var regenTimer: Timer
-var regenDelayTimer: Timer
-var healthBar: ProgressBar  # 血条节点
+var attrackedTimer: Timer
 
 signal healthChanged(health: int)
+signal destroyed()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,58 +27,49 @@ func _ready():
 	# 创建并启动计时器
 	regenTimer = Timer.new()
 	regenTimer.connect("timeout", Callable(self, "_onRegenTimerTimeout"))
-	regenTimer.wait_time = 1.0 / regenRate
+	regenTimer.wait_time = 1.0
 	regenTimer.one_shot = false
 	add_child(regenTimer)
 
-	regenDelayTimer = Timer.new()
-	regenDelayTimer.connect("timeout", Callable(self, "_onRegenDelayTimerTimeout"))
-	regenDelayTimer.wait_time = regenDelay
-	add_child(regenDelayTimer)
+	attrackedTimer = Timer.new()
+	attrackedTimer.connect("timeout", Callable(self, "_onAttrackedTimerTimeout"))
+	attrackedTimer.wait_time = regenDelay
+	add_child(attrackedTimer)
+	currentHealth = maxHealth
 	
-	startRegenDelayTimer()
-
-func startRegenDelayTimer():
+func resetAttrackedTimer():
 	# 未受攻击定时器关闭时开启, 开启时重置定时器
-	if regenDelayTimer.is_stopped():
-		regenDelayTimer.start()
+	if attrackedTimer.is_stopped():
+		attrackedTimer.start()
 	else:
-		regenDelayTimer.stop()
-		regenDelayTimer.start()
+		attrackedTimer.stop()
+		attrackedTimer.start()
+
+func stopRegenTimer():
 	if not regenTimer.is_stopped():
 		regenTimer.stop()
-		
-func _onRegenTimerTimeout():
-	heal(1)  # 每次回血1点
-	
-func _onRegenDelayTimerTimeout():
-	if currentHealth < maxHealth:
-		regenTimer.start()
-	
-func takeDamage(damage: int):
-	currentHealth -= damage
-	currentHealth = max(currentHealth, 0)
-	emit_signal("healthChanged", currentHealth)
-	updateHealthBar()
-	startRegenDelayTimer()
 
-func heal(amount: int):
-	currentHealth += amount
-	currentHealth = min(currentHealth, maxHealth)
+func _onRegenTimerTimeout():
+	# 每次回血regenRate点
+	plusHealth(regenRate)  
+	
+func _onattrackedTimerTimeout():
+	regenTimer.start()
+		
+	
+func minusHealth(amount: int):
+	currentHealth = clamp(currentHealth - amount, 0, maxHealth)
 	emit_signal("healthChanged", currentHealth)
-	updateHealthBar()
-	if currentHealth == maxHealth:
-		regenTimer.stop()
+	resetAttrackedTimer()
+	stopRegenTimer()
+
+func plusHealth(amount: int):
+	currentHealth = clamp(currentHealth + amount, 0, maxHealth)
+	emit_signal("healthChanged", currentHealth)
+	attrackedTimer.stop()
 
 func updateHealthBar():
-	if healthBar != null:
-		healthBar.value = currentHealth
-		if healthBar.value == maxHealth:
-			healthBar.visible = false
-		else:
-			healthBar.visible = true
-
-
-func setHealthBar(bar: ProgressBar):
-	healthBar = bar
-	updateHealthBar()
+	if healthBar == null:
+		return
+	healthBar.value = currentHealth
+	healthBar.visible = healthBar.value != maxHealth
