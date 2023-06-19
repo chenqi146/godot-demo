0a788e46e48a0186aed4fdb31ba5ad9eb59c9409
chenqi <1535985458@qq.com>
2023-06-20 00:17:12 +0800

    add tree resource

--- Objects/Pickups/Pickup.gd
+++ Objects/Pickups/Pickup.gd
@@ -1,13 +1,37 @@
-extends StaticBody2D
+extends Area2D
 
 class_name  Pickup
 
+@onready var collision_shape : CollisionShape2D = $CollisionShape2D
+@export var resource_type : Resource
 
-# Called when the node enters the scene tree for the first time.
+var launch_velocity : Vector2 = Vector2.ZERO
+var move_duration : float = 0
+var time_since_launch : float = 0
+var launching : bool = false :
+	set(is_launching):
+		launching = is_launching
+		collision_shape.disabled = launching
+
 func _ready() -> void:
-	pass # Replace with function body.
+	connect("body_entered", _on_body_entered)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if launching:
+		position += launch_velocity * delta
+		time_since_launch += delta
+		if time_since_launch >= move_duration:
+			launching = false
+
+func launch(velocity: Vector2, duration : float):
+	launch_velocity = velocity
+	move_duration = duration
+	time_since_launch = 0
+	launching = true
+
+
+func _on_body_entered(body : Node2D):
+	var inventory = body.find_child("Inventory")
+	if inventory:
+		inventory.add_resources(resource_type, 1)
+		queue_free()
--- Scripts/ResourceNode.gd
+++ Scripts/ResourceNode.gd
@@ -5,8 +5,11 @@ class_name ResourceNode
 @export var node_types : Array[ResourceNodeType]
 @export var starting_resource : int 
 @export var pickup_type : PackedScene
+@export var launch_speed : float = 100
+@export var launch_duration : float = 0.25
 var current_resources : int :
 	set(value):
+		current_resources = value
 		if value <= 0:
 			queue_free()
 
@@ -24,3 +27,9 @@ func spawn_resource():
 	var pickup_instance : Pickup = pickup_type.instantiate() as Pickup
 	# 设置位置, 资源节点周围
 	get_parent().add_child(pickup_instance)
+	pickup_instance.position = position
+	var direction : Vector2 = Vector2(
+		randf_range(-1.0, 1.0),
+		randf_range(-1.0, 1.0)
+	).normalized()
+	pickup_instance.launch(direction * launch_speed, launch_duration)
